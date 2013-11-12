0cb94e2a77454ea882cf0216e6595955e981b65d
cemoody <chrisemoody@gmail.com>
2013-11-11 17:52:06 -0800

    probabilsitics vector dot calculation; 4x speedup.

--- shortdot.pyx
+++ shortdot.pyx
@@ -7,7 +7,8 @@ cimport numpy as np
 def shortdot(
     np.ndarray[float, ndim=2] A,
     np.ndarray[float, ndim=1] B,
-    np.ndarray[double, ndim=1] C,
+    np.ndarray[float, ndim=1] C,
+    int jmax = 50,
     float threshold = -3.0):
     '''
     Matrix-vector multiplication.
@@ -20,17 +21,28 @@ def shortdot(
         int A_n = A.shape[0]
         int A_m = A.shape[1]
         int B_n = B.shape[0]
-        double tot = 0.0
+        int C_n = C.shape[0]
+        float tot = 0.0
         int skipped = 0
+
+    assert A_m == B_n
+    assert C_n == A_n
     
     # Initialize the results matrix.
     for i in xrange(A_n):
+        tot = 0.0
         for j in xrange(A_m):
-            tot += A[i, j] * B[j]
-            if j % 10 == 9:
-                #print i, j, tot
-                if tot < threshold:
-                    skipped += B_n - j
-                    break
+            #tot += <double> (A[i, j] * B[j])
+            tot += (A[i, j] * B[j])
+            #if j % 10 == 0:
+            #    z = abs(tot - 1.0) /  (0.3) * j / jmax
+            #    if z < threshold:
+            #        skipped += B_n - j
+            #        break
+            if j > jmax and tot < threshold:
+            #if tot < threshold:
+                skipped += B_n - j
+                break
         C[i] = tot
+    #print i,j, A_n, A_m, B_n, C_n
     return skipped
--- shortdot_test.py
+++ shortdot_test.py
@@ -7,25 +7,46 @@ import numpy as np
 import shortdot
 import time
 
-rows = 100
-dims = 1000
-A = np.random.normal(size=(rows, dims)).astype('f4')
-B = np.random.normal(size=(dims)).astype('f4')
-C = np.zeros(dims)
+test_rand = False
 
-n = 500
+if test_rand:
+    rows = int(1e5)
+    dims = 1000
+    A = np.random.normal(size=(rows, dims)).astype('f4')
+    B = np.random.normal(size=(dims)).astype('f4')
+    C = np.zeros(dims)
+    thresh = -1.0
+    print "created matrix"
+else:
+    A = np.load("/nobackupp5/cmoody3/data/ids/trained/vectors.fullwiki.1000.s50.num.npy")
+    A = A.astype('f4')
+    B = A[A.shape[0]/2]
+    C = np.zeros(A.shape[0]).astype('f8')
+    thresh = 0.1
+    rows = A.shape[0]
+    dims = A.shape[1]
+C = C.astype('f4')
+n = 20
 start = time.time()
 for i in range(n):
-    skipped = shortdot.shortdot(A, B, C, -1.0)
+    C = np.zeros(rows).astype('f4')
+    skipped = shortdot.shortdot(A, B, C, 50, thresh)
 stop = time.time()
-print "finished cython, skipped %i"  % skipped
-cy = (stop - start) * 1.0 / n * 1e9
+frac = skipped * 1.0 / (rows * dims)
+print "finished cython, skipped %i, %1.1f%%"  % (skipped, frac * 100.0)
+cy = (stop - start) * 1.0 / n * 1e6
+print 'cython top 5:', np.sort(C)[-5:], np.argsort(C)[-5:]
 
 start = time.time()
 for i in range(n):
-    np.dot(A, B)
+    x = np.zeros(rows).astype('f4')
+    D = np.dot(A, B)
 stop = time.time()
-py = (stop - start) * 1.0 / n * 1e9
+py = (stop - start) * 1.0 / n * 1e6
+print 'numpy  top 5:', np.sort(D)[-5:], np.argsort(D)[-5:]
+comp = np.where(np.argsort(D)[::-1] != np.argsort(C)[::-1])[0]
+print "first inequal indices", comp[:10]
 
-print "cython: %1.3ens" % cy
-print "python: %1.3ens" % py
+print "cython: %1.3ems" % cy
+print "python: %1.3ems" % py
+print "cython speed up %1.1f " % (py / cy)
